662628b479d47e41c26cd96189edc9b5f3057ec5
Sylvain Gamel <sylvain@SGinTech.fr>
2019-06-02 22:02:58 +0200

    Initial Commit

---
+++ WebkitEntitlementDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WebkitEntitlementDemo
+//
+//  Created by Sylvain on 02/06/2019.
+//  Copyright © 2019 Automatisez.net. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ WebkitEntitlementDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WebkitEntitlementDemo
+//
+//  Created by Sylvain on 02/06/2019.
+//  Copyright © 2019 Automatisez.net. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
